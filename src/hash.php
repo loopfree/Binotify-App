e4375d028fca0f5c6e5244095e4e81f15e89e0e4
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-10-25 20:33:05 +0700

    feat(hash): add password hashing

--- src/hash.php
+++ src/hash.php
@@ -1,12 +1,30 @@
 <?php
 
-function hashString($string) {
+function hashUsername($string) {
     $hash = 0;
     for($i = 0; $i < strlen($string); ++$i) {
         $hash += ord($string[$i]) + $i;
     }
 
     return $hash;
+}
+
+function hashPasswordTo($password) {
+    $result = "";
+    for($i = 0; $i < strlen($password); ++$i) {
+        $result .= chr(ord($password) + 1);
+    }
+
+    return $result;
+}
+
+function hashPasswordFrom($password) {
+    $result = "";
+    for($i = 0; $i < strlen($password); ++$i) {
+        $result .= chr(ord($password) - 1);
+    }
+
+    return $result;
 }
 
 ?>
