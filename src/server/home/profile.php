829319e1f386fae0e59bee927be7353fe7492a21
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-10-28 09:05:28 +0700

    feat: fix guest session

--- src/server/home/profile.php
+++ src/server/home/profile.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    if(!isset($_SESSION['logged_in'])) {
-        header("Location: /index.php");
+    if(isset($_SESSION['logged_in'])) {
+        // header("Location: /index.php");
+        echo $_SESSION['username'];
     }
-    echo $_SESSION['username'];
 ?>
