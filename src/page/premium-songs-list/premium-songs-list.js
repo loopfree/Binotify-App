3476262495b7bbb0c653de5471baab697c109101
FelineJTD <16520335@std.stei.itb.ac.id>
2022-11-29 22:56:37 +0700

    feat: add premium songs page

---
+++ src/page/premium-songs-list/premium-songs-list.js
@@ -0,0 +1,57 @@
+window.onload = () => {
+    getNav();
+    getSongCards();
+    getProfile();
+}
+
+function getNav() {
+    const nav = document.getElementsByClassName('nav')[0];
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `/server/nav/nav.php`);
+    xhr.onload = function() {
+        nav.innerHTML = this.responseText;
+    }
+    xhr.send();
+}
+
+function getSongCards() {
+    const songContainer = document.getElementById("songs-container");
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "/server/home/index.php", true);
+    xhr.onload = function() {
+        songContainer.innerHTML = this.responseText;
+        songPlayUpdate();
+    }
+    xhr.send(null);
+}
+
+function getProfile() {
+    const profile = document.getElementById("profile");
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `/server/home/profile.php`);
+    xhr.onload = function() {
+        profile.innerHTML = this.responseText;
+    }
+    xhr.send();
+}
+
+function songPlayUpdate() {
+    const songCards = document.getElementsByClassName("song-card");
+
+    for (let i=0; i < songCards.length; i++) {
+        const songCard = songCards[i];
+        songCard.addEventListener("mouseenter", function() {
+            const playButton = songCard.querySelector(".play-button");
+            playButton.style.opacity = "100%";
+            playButton.style.transform = "translateY(0)";
+        })
+        songCard.addEventListener("mouseleave", function() {
+            const playButton = songCard.querySelector(".play-button");
+            playButton.style.opacity = "0";
+            playButton.style.transform = "translateY(0.5rem)";
+        })
+        songCard.addEventListener("click", function() {
+            window.location.href = `/page/detail-lagu/index.php?song-id=${songCard.getAttribute("song-id")}`;
+        })
+    }
+}
